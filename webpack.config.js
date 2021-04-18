de4315cfb0524f0aafc7907a1a02c959722754ec
ShibinRajuMathewSRM <shibinrajumathew@yahoo.com>
2021-04-18 22:20:25 +0530

    wepack added

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, "./public/index.html"),
+  filename: "index.html",
+});
+
+module.exports = {
+  mode: "production",
+  entry: ["@babel/polyfill", path.resolve(__dirname, "./src/index.js")],
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [htmlWebpackPlugin],
+};
